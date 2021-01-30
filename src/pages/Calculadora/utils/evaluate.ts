5d00cbe8e7f5af2f14884f5933ad0e727256cfde
Douglas Moura <douglas.ademoura@gmail.com>
2021-01-30 11:47:23 -0300

    feature: implementa a função que avalia as expressões em notação polonesa

---
+++ src/pages/Calculadora/utils/evaluate.ts
@@ -0,0 +1,35 @@
+import operadores from './operadores';
+
+const evaluate = (tokens: (string | number)[]): (number | string) => {
+  const head = tokens.shift();
+
+  if (head === undefined)
+    throw new Error('Token inválido');
+
+  if (head === '(') {
+    const firstElement = tokens.shift();
+    
+    if (firstElement === undefined)
+      throw new Error('Operador inválido');
+
+    // @ts-ignore
+    const op = operadores[firstElement];
+
+    if (op === undefined)
+      throw new Error('Operador não encontrado');
+
+    const args = [];
+
+    while (tokens[0] !== ')') {
+      args.push(evaluate(tokens));
+    }
+
+    tokens.shift(); // Remove o ')'
+    return op(args);
+
+  } else {
+    return head;
+  }
+}
+
+export default evaluate;
