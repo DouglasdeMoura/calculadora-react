344cd51064b5655b75c81d37c77c9f401ee2afd5
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-01 11:25:06 -0300

    chore: valida expressões infix antes de passar para a calculadora

--- src/pages/Calculadora/helpers/calculate.ts
+++ src/pages/Calculadora/helpers/calculate.ts
@@ -1,9 +1,10 @@
 import evaluate from '../utils/evaluate';
+import expressaoInfixValida from '../utils/expressaoInfixValida';
 import infixToPrefix from '../utils/infixToPrefix';
 import tokenizer from '../utils/tokenizer';
 
 const calculate = (expression: string): (number | string) => {
-  if (isNaN(parseInt(expression.slice(-1))))
+  if (!expressaoInfixValida(expression))
     throw new Error('Expressão inválida');
 
   expression = expression
