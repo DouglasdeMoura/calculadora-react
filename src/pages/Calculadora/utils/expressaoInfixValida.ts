27573b05892d880a1ded504c02867573685f663c
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-01 11:24:43 -0300

    feat: implementa validador de expressões infix

---
+++ src/pages/Calculadora/utils/expressaoInfixValida.ts
@@ -0,0 +1,34 @@
+const expressaoInfixValida = (expressao: string): boolean => {
+  // Valida se expressão não termina com operador
+  if (expressao.slice(-1) !== ')' && isNaN(parseInt(expressao.slice(-1))))
+    return false;
+
+  // Válida se todos os parênteses abertos são fechados posteriormente
+    const expressaoArr = expressao.split('');
+  let abreParenteses = 0;
+  let fechaParenteses = 0;
+  expressaoArr.forEach((item) => {
+    if (item === '(')
+      abreParenteses++;
+    else if (item === ')')
+      fechaParenteses++;
+  });
+
+  if (abreParenteses !== fechaParenteses)
+    return false;
+
+  // Valida se expressão termina com número
+  for (let i = expressao.length - 1; i > -1; i--) {
+    if (expressao[i] === ')')
+      continue;
+
+    if(isNaN(parseInt(expressao[i]))) {
+      return false;
+    }
+    break;
+  }
+
+  return true;
+}
+
+export default expressaoInfixValida;
