6a077e20fd988151fdd66f48985817723917bab5
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-01 13:11:32 -0300

    feat: adiciona suporte para interação com o teclado

--- src/pages/Calculadora/Calculadora.tsx
+++ src/pages/Calculadora/Calculadora.tsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import botoes from './constants/botoes';
 import Botao from './components/Botao/Botao';
 import Visor from './components/Visor/Visor';
 import { CalculadoraAcoes } from './types/index';
-import { BotoesContainer, CalculadoraContainer } from './StyledComponents';
+import { BotaoEscondidoParaAdicionarOFocoNaCalculadora, BotoesContainer, CalculadoraContainer } from './StyledComponents';
 import calculate from './helpers/calculate';
+import teclas from './constants/teclas';
 
 const Calculadora = () => {
   const [expressao, setExpressao] = useState('');
   const [resultado, setResultado] = useState('');
+  const calculadora = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    calculadora
+      .current
+      ?.querySelector('button')
+      ?.focus();
+  }, []);
 
   const calcular = (expressao: string) => {
     try {
@@ -29,9 +38,7 @@ const Calculadora = () => {
     setResultado(resultado.slice(0, -1));
   }
 
-  const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const botaoClicado = event.currentTarget.innerText;
-
+  const executarAcao =  (botaoClicado: string) => {
     if (botaoClicado === CalculadoraAcoes.calcular)
       return calcular(resultado);
 
@@ -41,13 +48,54 @@ const Calculadora = () => {
     if (botaoClicado === CalculadoraAcoes.apagarUltimoItem)
       return apagarUltimoItem();
 
-    setResultado(`${resultado}${botaoClicado}`.replace('s', ' '));
+    setResultado(`${resultado}${botaoClicado}`);
+  }
+
+  const handleOnClickAndSetFocus = () => {
+    calculadora
+      .current
+      ?.querySelector('button')
+      ?.focus();
+  }
+
+  const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const botaoClicado = event.currentTarget.innerText;
+    executarAcao(botaoClicado);
+    event.currentTarget.blur();
+  }
+
+  const handleOnKeyDown = (event: React.KeyboardEvent) => {
+    let tecla = event.key;
+
+    if (tecla === 'Backspace')
+      tecla = CalculadoraAcoes.apagarUltimoItem;
+    else if (tecla === 'Delete')
+      tecla = CalculadoraAcoes.limparVisor;
+    else if (tecla === 'Enter')
+      tecla = CalculadoraAcoes.calcular;
+    else if (tecla === '/')
+      tecla = CalculadoraAcoes.dividir;
+    else if (tecla === '*')
+      tecla = CalculadoraAcoes.multiplicar;
+    else if (tecla === 'c')
+      tecla = CalculadoraAcoes.limparVisor;
+    else if (tecla === ',')
+      tecla = '.';
+
+    if (teclas.includes(tecla))
+      executarAcao(tecla);
   }
 
   return (
-    <CalculadoraContainer>
+    <CalculadoraContainer
+      id="calculadora"
+      onKeyDown={handleOnKeyDown}
+      onClick={handleOnClickAndSetFocus}
+      ref={calculadora}
+    >
       <Visor expressao={expressao} resultado={resultado} />
       <BotoesContainer>
+        <BotaoEscondidoParaAdicionarOFocoNaCalculadora />
         {
           botoes.map(botao => (
             <Botao
--- src/pages/Calculadora/StyledComponents/index.tsx
+++ src/pages/Calculadora/StyledComponents/index.tsx
@@ -14,3 +14,17 @@ export const BotoesContainer = styled.div`
   grid-template-columns: repeat(4, 1fr);
   gap: 8px;
 `;
+
+export const BotaoEscondidoParaAdicionarOFocoNaCalculadora = styled.button`
+  border: 0 !important;
+  clip: rect(1px, 1px, 1px, 1px) !important;
+  -webkit-clip-path: inset(50%) !important;
+  clip-path: inset(50%) !important;
+  height: 1px !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  padding: 0 !important;
+  position: absolute !important;
+  width: 1px !important;
+  white-space: nowrap !important;
+`;
